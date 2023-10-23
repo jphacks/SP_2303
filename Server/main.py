e482b0ac5c849b764cb87d541348031b1b452bd8
karintou8710 <karintou8710@gmail.com>
2023-10-23 18:07:12 +0900

    [fix] mount dir error

--- Server/main.py
+++ Server/main.py
@@ -1,9 +1,11 @@
+import os
+
 import firebase_admin
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from app.router import anonymous_post, swipe
-from app.settings import FIREBASE_CONFIG
+from app.settings import FIREBASE_CONFIG, SYSTEM_MEDIA_PATH
 
 ## 初期化 ##
 firebase_admin.initialize_app(firebase_admin.credentials.Certificate(FIREBASE_CONFIG))
@@ -16,4 +18,6 @@ app.include_router(anonymous_post.router)
 app.include_router(swipe.router)
 
 # 静的ファイルのマウント
+if not os.path.exists(SYSTEM_MEDIA_PATH):
+    os.makedirs(SYSTEM_MEDIA_PATH)
 app.mount("/media", StaticFiles(directory="app/media"), name="media")
