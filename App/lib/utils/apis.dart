95cee4e3a85d591edc91856188050865c540342e
Shingo TOYODA <toyodasinngo@gmail.com>
2023-10-24 22:04:55 +0900

    API通信のクラス作成

--- App/lib/utils/apis.dart
+++ App/lib/utils/apis.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:gohan_map/utils/logger.dart';
 import 'package:http/http.dart' as http;
+import 'package:isar/isar.dart';
 import 'package:latlong2/latlong.dart';
 
 http.Client client = http.Client(); // HTTPクライアントを格納する
 
+//placeAPI
 class PlaceApiRestaurantResult {
   final LatLng latlng;
   final String name;
@@ -54,4 +57,65 @@ Future<List<PlaceApiRestaurantResult>> searchRestaurantsByGoogleMapApi(
   }
 
   return result;
+}
+
+//SwipeUI用API
+class SwipeUIAPIResult {
+  final String name;
+  final String address;
+  final double latitude;
+  final double longitude;
+  final double star;
+  final byte image;
+
+  SwipeUIAPIResult(
+      {required this.name,
+      required this.address,
+      required this.latitude,
+      required this.longitude,
+      required this.star,
+      required this.image});
+
+  factory SwipeUIAPIResult.fromJson(Map<String, dynamic> data) {
+    String nameResult = data["name"];
+    String addressResult = data["address"];
+    double latitudeResult = data["latitude"];
+    double longitudeResult = data["longitude"];
+    double starResult = data["star"];
+    byte imageResult = data["image"];
+    return SwipeUIAPIResult(
+        name: nameResult,
+        address: addressResult,
+        latitude: latitudeResult,
+        longitude: longitudeResult,
+        star: starResult,
+        image: imageResult);
+  }
+}
+
+class APIService {
+  static Future<List<SwipeUIAPIResult>?> requestSwipeAPI(
+      LatLng latlng, int radius, String token) async {
+    final String apiUrl =
+        'https://gohanmap.almikan.com/api/swipe/anonymous-post?latitude=${latlng.latitude}&longitude=${latlng.longitude}&radius=$radius';
+    List<SwipeUIAPIResult> result = [];
+
+    try {
+      var response = await client.get(Uri.parse(apiUrl));
+      debugPrint(response.body);
+      if (response.statusCode != 200) throw "通信に失敗しました";
+
+      final responseData = json.decode(response.body);
+      if (responseData["status"] != "OK") throw "APIリクエストに失敗しました";
+
+      for (var item in responseData["results"]) {
+        result.add(SwipeUIAPIResult.fromJson(item));
+      }
+    } catch (e) {
+      logger.e(e);
+      return null;
+    }
+
+    return result;
+  }
 }
--- App/lib/view/swipeui_pre_page.dart
+++ App/lib/view/swipeui_pre_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gohan_map/utils/apis.dart';
 import 'package:gohan_map/view/swipeui_page.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class SwipeUIPrePage extends StatefulWidget {
   const SwipeUIPrePage({super.key});
@@ -23,12 +26,17 @@ class SwipeUIPrePageState extends State<SwipeUIPrePage> {
             children: [
               ElevatedButton(
                 child: const Text('SwipeUI'),
-                onPressed: () {
-                  Navigator.push(
+                onPressed: () async {
+                  //現在地を取得
+                  var currentLocation = await getCurrentLocation();
+                  await APIService.requestSwipeAPI(currentLocation, 100000, "");
+                  if (mounted) {
+                    Navigator.push(
                     context,
                     MaterialPageRoute(
                         builder: (context) => const SwipeUIPage()),
                   );
+                  }
                 },
               ),
             ],
@@ -36,5 +44,13 @@ class SwipeUIPrePageState extends State<SwipeUIPrePage> {
         ),
       ),
     );
+  }
+
+  Future<LatLng> getCurrentLocation() async {
+    //現在地を取
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    LatLng currentLocation = LatLng(position.latitude, position.longitude);
+    return currentLocation;
   }
 }
