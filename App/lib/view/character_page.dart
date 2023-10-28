38a83c13c965dd57d76b484204d1d9ea259d291e
usatyo <yu0424.math8128@gmail.com>
2023-10-28 18:29:54 +0900

    fix character page design

--- App/lib/view/character_page.dart
+++ App/lib/view/character_page.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:path/path.dart' as p;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +7,7 @@ import 'package:gohan_map/colors/app_colors.dart';
 import 'package:gohan_map/utils/common.dart';
 import 'package:gohan_map/utils/isar_utils.dart';
 import 'package:gohan_map/view/post_detail_page.dart';
+import 'package:path/path.dart' as p;
 
 class CharacterPage extends StatefulWidget {
   const CharacterPage({super.key});
@@ -51,183 +51,238 @@ class CharacterPageState extends State<CharacterPage> {
     }
 
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 60),
-          const Text("ステータス"),
-          Image.asset(
-            "images/normal.png",
-            height: 300,
-          ),
-          Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      child: Container(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          children: [
+            const SizedBox(height: 60),
+            const Column(
               children: [
-                Container(
-                  height: 25,
-                  width: 186,
-                  margin: const EdgeInsets.only(bottom: 20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: AppColors.primaryColor,
-                  ),
-                  child: const Text(
-                    "行ったお店の数",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.whiteColor,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 40,
-                ),
-                const Column(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Text(
-                      "150",
-                      style:
-                          TextStyle(fontSize: 36, fontWeight: FontWeight.w800),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "レベル",
+                          style: TextStyle(
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          "21",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.w600),
+                        )
+                      ],
                     ),
-                    Text(
-                      "(今月 +20)",
-                      style: TextStyle(fontSize: 16),
-                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "2320",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(width: 4),
+                        Text(
+                          "/",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(width: 3),
+                        Text(
+                          "2500",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    )
                   ],
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 25,
-                  width: 186,
-                  margin: const EdgeInsets.only(bottom: 20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: AppColors.primaryColor,
-                  ),
-                  child: const Text(
-                    "記録した数",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.whiteColor,
-                    ),
-                  ),
                 ),
-                const SizedBox(
-                  width: 40,
-                ),
-                const Column(
-                  children: [
-                    Text(
-                      "300",
-                      style:
-                          TextStyle(fontSize: 36, fontWeight: FontWeight.w800),
-                    ),
-                    Text(
-                      "(今月 +50)",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            const Row(
-              children: [
-                Icon(
-                  Icons.restaurant,
-                  size: 24,
+                SizedBox(height: 2),
+                LinearProgressIndicator(
+                  value: 2320 / 2500,
                   color: AppColors.primaryColor,
-                ),
-                Text(
-                  "食べたもの記録",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                  backgroundColor: AppColors.greyColor,
+                  minHeight: 14,
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
                 )
               ],
             ),
-            //Gridで画像を表示
-            FutureBuilder(
-                future: getLocalPath(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState != ConnectionState.done) {
-                    return Container();
-                  } else {
-                    //画像がある投稿のみを表示
-                    if (shopTimelineWithImg.isEmpty) {
-                      return Container(
-                        padding: const EdgeInsets.all(16),
-                        child: const Text("投稿がありません"),
+            Image.asset(
+              "images/normal_close.png",
+              height: 300,
+            ),
+            Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 25,
+                    width: 186,
+                    margin: const EdgeInsets.only(bottom: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: AppColors.primaryColor,
+                    ),
+                    child: const Text(
+                      "行ったお店の数",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.whiteColor,
+                      ),
+                    ),
+                  ),
+                  const Column(
+                    children: [
+                      Text(
+                        "150",
+                        style: TextStyle(
+                            fontSize: 36, fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        "(今月 +20)",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 25,
+                    width: 186,
+                    margin: const EdgeInsets.only(bottom: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: AppColors.primaryColor,
+                    ),
+                    child: const Text(
+                      "記録した数",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.whiteColor,
+                      ),
+                    ),
+                  ),
+                  const Column(
+                    children: [
+                      Text(
+                        "300",
+                        style: TextStyle(
+                            fontSize: 36, fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        "(今月 +50)",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 40),
+              const Row(
+                children: [
+                  Icon(
+                    Icons.restaurant,
+                    size: 24,
+                    color: AppColors.primaryColor,
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    "食べたもの記録",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+              //Gridで画像を表示
+              FutureBuilder(
+                  future: getLocalPath(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      return Container();
+                    } else {
+                      //画像がある投稿のみを表示
+                      if (shopTimelineWithImg.isEmpty) {
+                        return Container(
+                          padding: const EdgeInsets.all(16),
+                          child: const Text("投稿がありません"),
+                        );
+                      }
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: shopTimelineWithImg.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          mainAxisSpacing: 2,
+                          crossAxisSpacing: 2,
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 14),
+                        itemBuilder: (BuildContext context, int index) {
+                          return InkWell(
+                            onTap: () async {
+                              final s = shopTimelineWithImg[index].shopId;
+                              final shop = await IsarUtils.getShopById(s);
+                              if (mounted && shop != null) {
+                                Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                    builder: (context) => PostDetailPage(
+                                      timeline: shopTimeline![index],
+                                      imageData: snapshot.data,
+                                      shop: shop,
+                                    ),
+                                  ),
+                                ).then((value) => {
+                                      if (value == "delete")
+                                        {
+                                          setState(() {
+                                            shopTimeline!.remove(
+                                                shopTimelineWithImg[index]);
+                                          })
+                                        }
+                                    });
+                              }
+                            },
+                            child: Stack(fit: StackFit.expand, children: [
+                              Image.file(
+                                File(p.join(snapshot.data!,
+                                    shopTimelineWithImg[index].images[0])),
+                                fit: BoxFit.cover,
+                              ),
+                              if (shopTimelineWithImg[index].images.length >= 2)
+                                const Positioned(
+                                    top: 8,
+                                    right: 4,
+                                    child: Icon(
+                                      Icons.file_copy,
+                                      size: 23,
+                                      color: AppColors.whiteColor,
+                                    ))
+                            ]),
+                          );
+                        },
                       );
                     }
-                    return GridView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: shopTimelineWithImg.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        mainAxisSpacing: 2,
-                        crossAxisSpacing: 2,
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 14),
-                      itemBuilder: (BuildContext context, int index) {
-                        return InkWell(
-                          onTap: () async {
-                            final s = shopTimelineWithImg[index].shopId;
-                            final shop = await IsarUtils.getShopById(s);
-                            if (mounted && shop != null) {
-                              Navigator.push(
-                                context,
-                                CupertinoPageRoute(
-                                  builder: (context) => PostDetailPage(
-                                    timeline: shopTimeline![index],
-                                    imageData: snapshot.data,
-                                    shop: shop,
-                                  ),
-                                ),
-                              ).then((value) => {
-                                    if (value == "delete")
-                                      {
-                                        setState(() {
-                                          shopTimeline!.remove(
-                                              shopTimelineWithImg[index]);
-                                        })
-                                      }
-                                  });
-                            }
-                          },
-                          child: Stack(fit: StackFit.expand, children: [
-                            Image.file(
-                              File(p.join(snapshot.data!,
-                                  shopTimelineWithImg[index].images[0])),
-                              fit: BoxFit.cover,
-                            ),
-                            if (shopTimelineWithImg[index].images.length >= 2)
-                              const Positioned(
-                                  top: 8,
-                                  right: 4,
-                                  child: Icon(
-                                    Icons.file_copy,
-                                    size: 23,
-                                    color: AppColors.whiteColor,
-                                  ))
-                          ]),
-                        );
-                      },
-                    );
-                  }
-                }),
-          ])
-        ],
+                  }),
+            ])
+          ],
+        ),
       ),
     );
   }
