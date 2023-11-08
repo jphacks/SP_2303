8cd02043348e1b7d36bddeb59558284e496c6512
AlMikan <nekomimi987@gmail.com>
2023-11-08 18:09:24 +0900

    user削除apiの実装

---
+++ Server/app/router/user.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, Form, HTTPException, UploadFile, status
+from sqlalchemy.orm import Session
+
+from app.crud import user
+from app.db import get_db
+from app.dependency import get_current_user
+from app.types.fireabase import UserInfo
+from app.utils.logger import get_logger
+
+router = APIRouter()
+
+logger = get_logger()
+
+
+@router.delete("/api/user/withdraw", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_anonymous_post_and_image(
+    db: Session = Depends(get_db),
+    cred: UserInfo = Depends(get_current_user),
+) -> None:
+    logger.debug("request: DELETE /api/user/withdraw")
+    uid = cred["uid"]
+    post = user.fetch_anonymous_post_by_uid(
+        db,
+        uid,
+    )
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+
+    user.delete_anonymous_post_image_by_uid(
+        db,
+        uid,
+    )
+    user.delete_anonymous_post_by_uid(
+        db,
+        uid,
+    )
--- Server/main.py
+++ Server/main.py
@@ -4,7 +4,7 @@ import firebase_admin
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.router import anonymous_post, swipe
+from app.router import anonymous_post, swipe, user
 from app.settings import FIREBASE_CONFIG, SYSTEM_MEDIA_PATH
 
 ## 初期化 ##
@@ -16,6 +16,7 @@ app = FastAPI()
 # ルーティングの設定
 app.include_router(anonymous_post.router)
 app.include_router(swipe.router)
+app.include_router(user.router)
 
 # 静的ファイルのマウント
 if not os.path.exists(SYSTEM_MEDIA_PATH):
